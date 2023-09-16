5af8ba4e25f5d05d1a1c86424c84af4da7219b51
leandrovelosos <leandrovdsantos96@gmail.com>
2023-09-16 19:45:51 -0300

    validacao de dados

--- js/app.js
+++ js/app.js
@@ -7,6 +7,16 @@ class Despesa{
         this.descricao = descricao
         this.valor = valor
     }
+
+    validarDados(){
+        for(let i in this){
+            if(this[i] == undefined || this[i] == null || this[i] == ''){
+                return false
+            }
+        }
+
+        return true 
+    }
 }
 
 class Bd {
@@ -73,7 +83,13 @@ function cadastrarDespesa(){
         descricao.value,
         valor.value)
 
-        bd.gravar(despesa)
+        if(despesa.validarDados()){
+           // bd.gravar(despesa)
+           console.log('dados validos')
+        }else{
+            console.log('dados invalidos')
+        }
+       
 }
 
 
